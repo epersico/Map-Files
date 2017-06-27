f7b7850df0fa1b9c8008438b077db48a91de18a7
epersico <elliot.persico@gmail.com>
2017-06-27 17:05:21 -0500

    Before output redo

---
+++ Resonance Widths/ResonanceWidths/ResonanceWidths/stdMapWidths.h
@@ -0,0 +1,14 @@
+//
+//  stdMapWidths.h
+//  ResonanceWidths
+//
+//  Created by Elliot Persico on 6/2/17.
+//  Copyright © 2017 Elliot Persico. All rights reserved.
+//
+
+#ifndef stdMapWidths_h
+#define stdMapWidths_h
+
+#include <stdio.h>
+
+#endif /* stdMapWidths_h */
